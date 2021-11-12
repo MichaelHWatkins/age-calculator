3943a64e5fc3bf08edfab00a56386229d6d0cec7
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-11-12 03:05:17 -0800

    complete the sixth test

--- src/age-calulator.js
+++ src/age-calulator.js
@@ -19,4 +19,8 @@ export default class Calculator {
   jupiter() {
     return Math.ceil((this.age / 11.86) * 100)/100;
   }
+
+  impendingDeath() {
+    return `On Mercury you have roughly ${Math.ceil( (Math.ceil((this.life / .24) * 100)/100) - (Math.ceil((this.age / .24) * 100)/100))} year left to live. On Venus you have ${Math.ceil( (Math.ceil((this.life / .62) * 100)/100) - (Math.ceil((this.age / .62) * 100)/100))} years left to live. On Mars you have ${Math.ceil( (Math.ceil((this.life / 1.88) * 100)/100) - (Math.ceil((this.age / 1.88) * 100)/100))} years left to live. And on Jupiter you have ${Math.ceil( (Math.ceil((this.life / 11.86) * 100)/100) - (Math.ceil((this.age / 11.86) * 100)/100))} years left to live.`
+  }
 }
