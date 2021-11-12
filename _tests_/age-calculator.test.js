bfe04f7b6307c68e4946b3fa4f8c066f073d48b9
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-11-12 02:55:19 -0800

    add the sixth test

--- _tests_/age-calculator.test.js
+++ _tests_/age-calculator.test.js
@@ -27,4 +27,9 @@ describe('Calulator', () => {
     const calulator = new Calculator(2, 95);
     expect(calulator.jupiter()).toEqual(.17);
   });
+
+  test('should correctly calculate the amount of time the user would have left on each planet assuming they make it to the expected life expectancy', () => {
+    const calulator = new Calculator(2, 95);
+    expect(calulator.impendingDeath()).toEqual("On Mercury you have roughly 388 year left to live. On Venus you have 150 years left to live. On Mars you have 50 years left to live. And on Jupiter you have 8 years left to live.");
+  })
 });
