00898f2dbf5df76d300de4ee44f09077133193f5
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-11-12 06:03:16 -0800

    complete the seventh test

--- src/age-calulator.js
+++ src/age-calulator.js
@@ -22,14 +22,10 @@ export default class Calculator {
 
   impendingDeath() {
     if (this.life > this.age) {
-      return `On Mercury you have roughly ${Math.ceil( (Math.ceil((this.life / .24) * 100)/100) - (Math.ceil((this.age / .24) * 100)/100))} year left to live.
-       On Venus you have ${Math.ceil( (Math.ceil((this.life / .62) * 100)/100) - (Math.ceil((this.age / .62) * 100)/100))} years left to live. 
-       On Mars you have ${Math.ceil( (Math.ceil((this.life / 1.88) * 100)/100) - (Math.ceil((this.age / 1.88) * 100)/100))} years left to live. 
-       And on Jupiter you have ${Math.ceil( (Math.ceil((this.life / 11.86) * 100)/100) - (Math.ceil((this.age / 11.86) * 100)/100))} years left to live.`
+      return `On Mercury you have roughly ${Math.ceil( (Math.ceil((this.life / .24) * 100)/100) - (Math.ceil((this.age / .24) * 100)/100))} year left to live. On Venus you have ${Math.ceil( (Math.ceil((this.life / .62) * 100)/100) - (Math.ceil((this.age / .62) * 100)/100))} years left to live. On Mars you have ${Math.ceil( (Math.ceil((this.life / 1.88) * 100)/100) - (Math.ceil((this.age / 1.88) * 100)/100))} years left to live. And on Jupiter you have ${Math.ceil( (Math.ceil((this.life / 11.86) * 100)/100) - (Math.ceil((this.age / 11.86) * 100)/100))} years left to live.`
     }
     else {
-      return `You have lived roughly ${Math.ceil((Math.ceil((this.age / .24) * 100)/100) - (Math.ceil((this.life / .24) * 100)/100))} years past your life expectancy on Mercury, 
-      ${Math.ceil( (Math.ceil((this.age / .62) * 100)/100) - (Math.ceil((this.life / .62) * 100)/100))} years past life expectancy on Venus, ${Math.ceil( (Math.ceil((this.age / 1.88) * 100)/100) - (Math.ceil((this.life / 1.88) * 100)/100))} year past life expectancy on Mars, and ${Math.floor( (Math.ceil((this.age / 11.86) * 100)/100) - (Math.ceil((this.life / 11.86) * 100)/100))} years past life expectancy on Jupiter!`
+      return `You have lived roughly ${Math.ceil((Math.ceil((this.age / .24) * 100)/100) - (Math.ceil((this.life / .24) * 100)/100))} years past your life expectancy on Mercury, ${Math.ceil( (Math.ceil((this.age / .62) * 100)/100) - (Math.ceil((this.life / .62) * 100)/100))} years past life expectancy on Venus, ${Math.ceil( (Math.ceil((this.age / 1.88) * 100)/100) - (Math.ceil((this.life / 1.88) * 100)/100))} year past life expectancy on Mars, and ${Math.floor( (Math.ceil((this.age / 11.86) * 100)/100) - (Math.ceil((this.life / 11.86) * 100)/100))} years past life expectancy on Jupiter!`
     }
   }
 }
