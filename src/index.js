b8180ddde62696afa077b174b13ffcfeff8a8c3a
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-11-12 06:23:44 -0800

    add ui logic

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import Triangle from './triangle.js';
+import Calculator from './age-calulator.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
-import 'bootstrap';
 $(document).ready(function() {
   $('#triangle-checker-form').submit(function(event) {
     event.preventDefault();
-    const length1 = $('#length1').val();
-    const length2 = $('#length2').val();
-    const length3 = $('#length3').val();
-    const triangle = new Triangle(length1, length2, length3)
-    const response = triangle.checkType();
-    $('#response').append("<p>" + response + "</p>");
+    const age = $('#age').val();
+    const lifespan = $('#lifespan').val();
+    const calculator = new Calculator(age, lifespan)
+    const mercury = calculator.mercury();
+    const venus = calculator.venus();
+    const mars = calculator.mars();
+    const jupiter = calculator.jupiter();
+    const impendingDeath = calculator.impendingDeath();
+    $('#response').append("<p>" + "Age on Mercury: " + mercury + ". Age on Venus: " + venus + ". Age on Mars: " + mars + ". Age on Jupiter: " + jupiter + "</p>");
+    $('#output1').text(impendingDeath);
   });
 });
